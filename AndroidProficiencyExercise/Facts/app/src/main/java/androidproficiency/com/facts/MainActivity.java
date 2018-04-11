bd70863fdf4099dfe40f2164aec3f3d91c8c4ead
abhatnagar-infy <anubha_bhatnagar@infosys.com>
2018-04-11 21:28:12 +0530

    Initial Project files

---
+++ AndroidProficiencyExercise/Facts/app/src/main/java/androidproficiency/com/facts/MainActivity.java
@@ -0,0 +1,43 @@
+package androidproficiency.com.facts;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings)
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://github.com/erikcaffrey/RecyclerView-Examples")));
+
+        return super.onOptionsItemSelected(item);
+    }
+}
